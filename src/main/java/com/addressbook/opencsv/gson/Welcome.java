77b673517d07744c2616975a45d287acc6896e35
123akg <abhijeetkumargiri7070@gmail.com>
2020-11-03 23:32:33 +0530

    Added: Welcome Message

---
+++ src/main/java/com/addressbook/opencsv/gson/Welcome.java
@@ -0,0 +1,10 @@
+package com.addressbook.opencsv.gson;
+
+public class Welcome {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Address Book Program!!!");
+	}
+
+}
